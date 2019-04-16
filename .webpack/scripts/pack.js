23e53c8f41e090d0c98a08c8b1f4407660a7b02c
idnesdotlink <16096613+idnesdotlink@users.noreply.github.com>
2019-04-16 17:36:46 +0700

    ok

--- .webpack/scripts/pack.js
+++ .webpack/scripts/pack.js
@@ -3,10 +3,21 @@ import { join } from 'path';
 import { src_path } from '../constants/paths';
 
 const lib_path = join(src_path, 'lib', 'color-picker', 'package.json');
+const lib_path2 = join(src_path, 'lib', 'svg-chart', 'package.json');
 
 ngPackage
   .ngPackagr()
   .forProject(lib_path)
+  // .withTsConfig('tsconfig.lib.json')
+  .build()
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
+ngPackage
+  .ngPackagr()
+  .forProject(lib_path2)
   // .withTsConfig('tsconfig.lib.json')
   .build()
   .catch(error => {
