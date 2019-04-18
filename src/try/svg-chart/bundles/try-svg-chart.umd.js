43599097896f2d1e317d99e0cd979d893f5273a8
idnesdotlink <16096613+idnesdotlink@users.noreply.github.com>
2019-04-19 04:36:52 +0700

    psg

--- src/try/svg-chart/bundles/try-svg-chart.umd.js
+++ src/try/svg-chart/bundles/try-svg-chart.umd.js
@@ -5,77 +5,149 @@
 }(this, function (exports, core, common, animations, platformBrowser, rxjs, operators, d3Array, d3Scale, d3Brush, d3Selection, d3Shape, d3Interpolate, d3Format, d3TimeFormat) { 'use strict';
 
     /*! *****************************************************************************
+
     Copyright (c) Microsoft Corporation. All rights reserved.
+
     Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+
     this file except in compliance with the License. You may obtain a copy of the
+
     License at http://www.apache.org/licenses/LICENSE-2.0
 
+
+
     THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+
     KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+
     WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+
     MERCHANTABLITY OR NON-INFRINGEMENT.
 
+
+
     See the Apache Version 2.0 License for specific language governing permissions
+
     and limitations under the License.
+
     ***************************************************************************** */
+
     /* global Reflect, Promise */
 
+
+
     var extendStatics = function(d, b) {
+
         extendStatics = Object.setPrototypeOf ||
+
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+
             function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+
         return extendStatics(d, b);
+
     };
 
+
+
     function __extends(d, b) {
+
         extendStatics(d, b);
+
         function __() { this.constructor = d; }
+
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+
     }
+
+
 
     function __decorate(decorators, target, key, desc) {
+
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+
         else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+
         return c > 3 && r && Object.defineProperty(target, key, r), r;
+
     }
+
+
 
     function __metadata(metadataKey, metadataValue) {
+
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(metadataKey, metadataValue);
+
     }
+
+
 
     function __values(o) {
+
         var m = typeof Symbol === "function" && o[Symbol.iterator], i = 0;
+
         if (m) return m.call(o);
+
         return {
+
             next: function () {
+
                 if (o && i >= o.length) o = void 0;
+
                 return { value: o && o[i++], done: !o };
+
             }
+
         };
+
     }
+
+
 
     function __read(o, n) {
+
         var m = typeof Symbol === "function" && o[Symbol.iterator];
+
         if (!m) return o;
+
         var i = m.call(o), r, ar = [], e;
+
         try {
+
             while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+
         }
+
         catch (error) { e = { error: error }; }
+
         finally {
+
             try {
+
                 if (r && !r.done && (m = i["return"])) m.call(i);
+
             }
+
             finally { if (e) throw e.error; }
+
         }
+
         return ar;
+
     }
 
+
+
     function __spread() {
+
         for (var ar = [], i = 0; i < arguments.length; i++)
+
             ar = ar.concat(__read(arguments[i]));
+
         return ar;
+
     }
 
     // The export is needed here to generate a valid polyfills.metadata.json file
